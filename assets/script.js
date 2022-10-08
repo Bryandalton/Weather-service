231dfe2d97e57814553d685f4aff2851ab887462
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-10-08 13:24:38 -0400

    structutre and fetchrequest

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,8 @@
-var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dcae681b7023d997abdb633d1b521fe8';
+var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dcae681b7023d997abdb633d1b521fe8';
 var apikey = 'dcae681b7023d997abdb633d1b521fe8';
+var weatherEl = document.querySelector('.weather ')
+
+
 
 fetch(requestUrl)
     .then(function (response) {
