5e085fb6613d5e45cc0b327751c2759207e17eb2
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-10-08 10:36:52 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dcae681b7023d997abdb633d1b521fe8';
+var apikey = 'dcae681b7023d997abdb633d1b521fe8';
+
+fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+    }); 
