5fee3c70f5c21ed10944a9dbaa4bbe4ee0b5889a
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-10-09 14:20:48 -0400

    fixed weatherEL

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dcae681b7023d997abdb633d1b521fe8';
+var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Richmond,us&APPID=dcae681b7023d997abdb633d1b521fe8';
 var apikey = 'dcae681b7023d997abdb633d1b521fe8';
 var weatherEl = document.querySelector('.weather')
 var city = document.querySelector('#city');
@@ -13,13 +13,16 @@ fetch(requestUrl)
     .then((data) => {
       console.log(data)
       weatherData = data;
+      weather()
+
     });
 
-console.log(weatherData)
+console.log()
 
-function weather (weatherData) {
+function weather () {
   console.log(weatherData)
   city.textContent = weatherData.name;
+  temp.textContent = 'Temp: ' + weatherData.main.temp
+  wind.textContent = 'Winds: ' + weatherData.wind.speed + ' to ' + weatherData.wind.gust;
 
 }
-weather()
