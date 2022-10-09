21b9dadf3c6fa36d902ab95f7034f061c8f1b95f
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-10-09 11:24:51 -0400

    weather section and js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,25 @@
 var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=dcae681b7023d997abdb633d1b521fe8';
 var apikey = 'dcae681b7023d997abdb633d1b521fe8';
-var weatherEl = document.querySelector('.weather ')
-
-
+var weatherEl = document.querySelector('.weather')
+var city = document.querySelector('#city');
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var humidity = document.querySelector('#humidity');
+var uv = document.querySelector('#humidity')
+var weatherData;
 
 fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data)
-    }); 
+      weatherData = data;
+    });
+
+console.log(weatherData)
+
+function weather (weatherData) {
+  console.log(weatherData)
+  city.textContent = weatherData.name;
+
+}
+weather()
