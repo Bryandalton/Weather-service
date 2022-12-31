7fd28ee5139f8d03c1077f59417002c1818db158
[Bryan Dalton] <[bryantdalton19@gmail.com]>
2022-12-31 13:54:01 -0500

    temp now F, added clouds and humidity, location buttons added

--- assets/script.js
+++ assets/script.js
@@ -1,28 +1,33 @@
-var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=Richmond,us&APPID=dcae681b7023d997abdb633d1b521fe8';
-var apikey = 'dcae681b7023d997abdb633d1b521fe8';
-var weatherEl = document.querySelector('.weather')
-var city = document.querySelector('#city');
-var temp = document.querySelector('#temp');
-var wind = document.querySelector('#wind');
-var humidity = document.querySelector('#humidity');
-var uv = document.querySelector('#humidity')
+var apiKey = "dcae681b7023d997abdb633d1b521fe8";
+var requestUrl = `http://api.openweathermap.org/data/2.5/weather?q=Richmond,us&APPID=${apiKey}`;
+var weatherEl = document.querySelector(".weather");
+var city = document.querySelector("#city");
+var temp = document.querySelector("#temp");
+var wind = document.querySelector("#wind");
+var humidity = document.querySelector("#humidity");
+var uv = document.querySelector("#humidity");
+var clouds = document.querySelector("#clouds");
+var searchBar = document.querySelector('#searchBar');
 var weatherData;
 
 fetch(requestUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      weatherData = data;
-      weather()
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    weatherData = data;
+    weather();
+  });
 
-    });
+console.log();
 
-console.log()
+function weather() {
+var tempF = Math.round(((weatherData.main.temp-273.15)*1.8)+32)
 
-function weather () {
-  console.log(weatherData)
+  // console.log(weatherData)
   city.textContent = weatherData.name;
-  temp.textContent = 'Temp: ' + weatherData.main.temp
-  wind.textContent = 'Winds: ' + weatherData.wind.speed + ' to ' + weatherData.wind.gust;
+  temp.textContent = `Temperature: ${tempF}℉`;
 
+  wind.textContent = `Wind Speed: ${weatherData.wind.speed}mph`
+  clouds.textContent = `${weatherData.weather[0].main}: ${weatherData.weather[0].description}`;
+  humidity.textContent= `Humidity: ${weatherData.main.humidity}%`
 }
